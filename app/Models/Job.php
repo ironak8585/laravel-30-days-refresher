06e5164dc57d16d67a6613848a13b295f386b36a
Ronak Ahir <ironak8585@gmail.com>
2024-10-13 19:52:58 +0530

    upto day 11

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,55 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Container\Attributes\Database;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 class Job extends Model
 {
+    use HasFactory;
+
     protected $table = 'job_listings';
 
-    protected $fillable = ['title', 'salary'];
+    protected $fillable = ['title', 'salary', 'employer_id'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
-
-
-// public static function all(): array
-//     {
-//         return     [
-//             [
-//                 'id' => 1,
-//                 'title' => 'Director',
-//                 'salary' => '40000'
-//             ],
-//             [
-//                 'id' => 2,
-//                 'title' => 'Programmer',
-//                 'salary' => '90000'
-//             ],
-//             [
-//                 'id' => 3,
-//                 'title' => 'Teacher',
-//                 'salary' => '30000'
-//             ],
-//         ];
-//     }
-
-//     /**
-//      * find the specific job
-//      */
-//     public static function find(int $id)
-//     {
-//         // $job = Arr::first($jobs, function ($job) use ($id) {
-//         //     return $job['id'] == $id;
-//         // });
-
-//          $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
-
-//         // return Arr::first(static::all(), fn($job) => $job['id'] == $id) ?? abort('404');
-
-//         $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-//         if (! $job) {
-//             abort('404');
-//         }
-//         return $job;
-//     }
--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -5,4 +5,7 @@
 
     <h1 class="text-2xl">Job Title is : {{ $job['title'] }}</h1>
     <p class="text-green-500">Salary {{ $job['salary'] }} per anum.</p>
+    <hr>
+    <h2 class="text-xl">Employer</h2>
+    <p class="text-red-500">{{ $job->employer->name }}</p>
 </x-layout>
